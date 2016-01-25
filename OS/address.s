c72fc94a4221d094eacbbd6a858968f32b044bec
shubhdev <shubhamrawat1100@gmail.com>
2016-01-25 16:50:12 +0530

    initial commit

---
+++ OS/address.s
@@ -0,0 +1,23 @@
+	.file	"address.cpp"
+.lcomm _ZL7shubham,1,1
+	.def	__main;	.scl	2;	.type	32;	.endef
+	.text
+	.globl	main
+	.def	main;	.scl	2;	.type	32;	.endef
+	.seh_proc	main
+main:
+.LFB0:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$32, %rsp
+	.seh_stackalloc	32
+	.seh_endprologue
+	call	__main
+	movl	$0, %eax
+	addq	$32, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.ident	"GCC: (GNU) 4.9.3"
