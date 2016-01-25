87de52f583123a4f7596fb611656b2c3e06b5e74
Shubham Rawat <shubhamrawat1100@gmail.com>
2016-01-25 17:05:00 +0530

    ubuntu commit

---
+++ OS/hw1/hello.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <string.h>
+#include <assert.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+void main(){
+	// char * const argv[] = {"/usr/bin/env","ls",0};
+	// int ret = execv(argv[0],argv);
+	
+	// printf("%s\n",strerror(errno));
+	int x;
+	scanf("%d",&x);
+	printf("%d",x);
+
+}
