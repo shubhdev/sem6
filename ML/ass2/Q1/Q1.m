1456b850d6263bad470cd224506d10e6e45095e8
Shubham Rawat <malashri.cse.iitd.ac.in>
2016-03-15 17:01:58 +0530

    GCL work, ML ass2

--- ML/ass2/Q1/Q1.m
+++ ML/ass2/Q1/Q1.m
@@ -1,18 +1,40 @@
 %%%%%%  PART A %%%%%
-train = importdata('train1.data',',');
-class = train(:,end);
-train = train(:,1:end-1);
-strain = sparse(train);
-lin_kernel = @(x1,x2)dot(x1,x2);
-bandwidth = 2.5*1e-4;
-gaussian_kernel = @(x1,x2)exp(-norm(x1-x2)/2*bandwidth^2);
-alph = SVM(lin_kernel,strain,class,1.0);
-
+X = importdata('train1.data',',');
+Y = X(:,end);
+X = X(:,1:end-1);
+%strain = sparse(train);
+lin_kernel = (Y*Y').*(X*X');
+%bandwidth = 2.5*1e-4;
+%gaussian_kernel = @(x1,x2)exp(-norm(x1-x2)/2*bandwidth^2);
+%alph = SVM(lin_kernel,Y,1.0);
+alph = load('dual_solution_gcl.txt','-ascii');
+threshold = 1e-5;
+cnt = 0;
+SV = [];
+for i = 1:length(Y)
+    if(alph(i) > threshold && alph(i) < 0.9999)
+        cnt = cnt+1;
+        %disp(Y(i));
+        SV = [SV;i];
+    end
+end
+%SV = load('sv_linear.txt','-ascii');
 %%%%%%% PART B  %%%%%%%%
 % w = sum(alph(i)*y(i)*x(i))
- m = length(class);
- W = zeros(1,m);
- for i = 1:m
-     W(i) = W(i) + alph(i)*class(i)*train(i,:);
- end
- W = W';
+  m = length(Y);
+  W = zeros(1,size(X,2));
+  for i = 1:m
+      W = W + (alph(i)*Y(i))*X(i,:);
+  end
+  W = W';
+
+  % calculate b, by using the margin vectors
+  b = 0;
+  for i = 1:length(SV)
+    idx = SV(i);
+    b_i = Y(idx) - X(idx,:)*W;
+    disp(b_i);
+    %fprintf(1,'%.4f %d\n',b_i,Y(i));
+    b = b + b_i;
+  end
+  b = b/length(SV);
--- ML/ass2/Q1/SVM.m
+++ ML/ass2/Q1/SVM.m
@@ -1,26 +1,25 @@
-function [alfa] = SVM(kernel,X,Y,C)
-m = size(Y,1);
-Q = zeros(m,m);
+function [alfa] = SVM(Q,Y,C)
+m = size(Q,1);
+%Q = zeros(m,m);
 disp(m);
 disp(C);
-for i = 1:m
-    for j = 1:m
-        %disp(i);
-        Q(i,j) = dot(X(i,:),X(j,:))*Y(i)*Y(j);
-    end
-end
-
+% for i = 1:m
+%     for j = 1:m
+%         %disp(i);
+%         Q(i,j) = dot(X(i,:),X(j,:))*Y(i)*Y(j);
+%     end
+% end
 %fprintf(1,'saving..\n');
-save('Q.mat','Q');
+%save('Q.mat','Q');
 %fprintf(1,'loading...\n');
 %load('Q.mat');
 B = ones(m,1);
 fprintf(1,'starting optimization\n');
 cvx_begin
     variable alfa(m);
-    maximize(-0.5.*alfa'*Q*alfa + B'*alfa);
+    minimize(0.5.*alfa'*Q*alfa - B'*alfa);
     subject to
-        0 <= alfa <= C;
+        0 <= alfa <= 1;
         Y'*alfa == 0;
 cvx_end
 end
